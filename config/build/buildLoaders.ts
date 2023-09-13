17626f76d618b0d4af4ac7b5c61def52c0459e7c
Tatiana Tsvet <tsvetynina@yandex.ru>
2023-09-13 14:45:32 +0300

    loader fixes

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,6 +1,6 @@
 import webpack from 'webpack';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { BuildOptions } from './types/config';
+import { buildCssLoader } from './loaders/buildCssLoader';
 
 export default function buildLoaders(options: BuildOptions):webpack.RuleSetRule[] {
     const { isDev } = options;
@@ -10,24 +10,7 @@ export default function buildLoaders(options: BuildOptions):webpack.RuleSetRule[
         use: ['@svgr/webpack'],
     };
 
-    const cssLoader = {
-        test: /\.s[ac]ss$/i,
-        use: [
-            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-            {
-                loader: 'css-loader',
-                options: {
-                    modules: {
-                        auto: (resPath: string) => Boolean(resPath.includes('.module.')),
-                        localIdentName: isDev
-                            ? '[path][name]__[local]--[hash:base64:5]'
-                            : '[hash:base64:8]',
-                    },
-                },
-            },
-            'sass-loader',
-        ],
-    };
+    const cssLoader = buildCssLoader(isDev);
 
     const babelLoader = {
         test: /\.(js|jsx|tsx)$/,
