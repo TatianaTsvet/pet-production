db03c73abd28d553132d5cbf4f3d1bc6fba13441
Tatiana Tsvet <tsvetynina@yandex.ru>
2023-09-13 13:27:11 +0300

    loaders fixes

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,6 +1,6 @@
 import webpack from 'webpack';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { BuildOptions } from './types/config';
-import { buildCssLoader } from './loaders';
 
 export default function buildLoaders(options: BuildOptions):webpack.RuleSetRule[] {
     const { isDev } = options;
@@ -8,6 +8,25 @@ export default function buildLoaders(options: BuildOptions):webpack.RuleSetRule[
     const svgLoader = {
         test: /\.svg$/,
         use: ['@svgr/webpack'],
+    };
+
+    const cssLoader = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            {
+                loader: 'css-loader',
+                options: {
+                    modules: {
+                        auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+                        localIdentName: isDev
+                            ? '[path][name]__[local]--[hash:base64:5]'
+                            : '[hash:base64:8]',
+                    },
+                },
+            },
+            'sass-loader',
+        ],
     };
 
     const babelLoader = {
@@ -29,8 +48,6 @@ export default function buildLoaders(options: BuildOptions):webpack.RuleSetRule[
             },
         },
     };
-
-    const cssLoader = buildCssLoader(isDev);
 
     const fileLoader = {
         test: /\.(png|jpe?g|gif|woff2|woff)$/i,
