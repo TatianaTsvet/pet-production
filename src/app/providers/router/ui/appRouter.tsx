b2a8010126bc59ddd1d72c90ae46cfe06969b90e
Tatiana Tsvet <tsvetynina@yandex.ru>
2023-09-13 12:46:51 +0300

    error fixes

--- src/app/providers/router/ui/appRouter.tsx
+++ src/app/providers/router/ui/appRouter.tsx
@@ -1,19 +1,30 @@
-import React, { Suspense } from 'react';
+import React, { FC, Suspense } from 'react';
+import { useTranslation } from 'react-i18next';
 import { Route, Routes } from 'react-router-dom';
 import { routeConfig } from 'shared';
 
-const AppRouter = () => (
-    <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-            {Object.values(routeConfig).map(({ element, path }) => (
-                <Route
-                    key={path}
-                    path={path}
-                    element={<div className="page-wrapper">{element}</div>}
-                />
-            ))}
-        </Routes>
-    </Suspense>
-);
+const AppRouter: FC = () => {
+    const { t } = useTranslation('main');
+
+    return (
+        <Suspense fallback={(
+            <div>
+                {t('page.loading')}
+                ...
+            </div>
+        )}
+        >
+            <Routes>
+                {Object.values(routeConfig).map(({ element, path }) => (
+                    <Route
+                        key={path}
+                        path={path}
+                        element={<div className="page-wrapper">{element}</div>}
+                    />
+                ))}
+            </Routes>
+        </Suspense>
+    );
+};
 
 export default AppRouter;
