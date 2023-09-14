3c5c2910a1f2b3c3fc972d6a413b2357f89535c2
Tatiana Tsvet <tsvetynina@yandex.ru>
2023-09-14 09:52:53 +0300

    added route tests

--- src/widgets/sidebar/ui/sidebar/sidebar.test.tsx
+++ src/widgets/sidebar/ui/sidebar/sidebar.test.tsx
@@ -1,16 +1,14 @@
-import { render, screen, fireEvent } from '@testing-library/react';
-import { withTranslation } from 'react-i18next';
-import React from 'react';
-import { renderWithTranslation } from 'shared/lib/tests';
+import { screen, fireEvent } from '@testing-library/react';
+import { componentRender } from 'shared/lib/tests';
 import { Sidebar } from './Sidebar';
 
 describe('sidebar', () => {
     test('with only one param', () => {
-        renderWithTranslation(<Sidebar />);
+        componentRender(<Sidebar />);
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     });
     test('test toggle', () => {
-        renderWithTranslation(<Sidebar />);
+        componentRender(<Sidebar />);
         const toggleBtn = screen.getByTestId('sidebar-toggle');
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
         fireEvent.click(toggleBtn);
